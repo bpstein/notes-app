b8f8ebe8b11b64eee04d15a7b018fe0db64ec718
Ben Stein <bumstain@gmail.com>
2015-08-02 14:06:07 +0100

    Added add and edit functionality for exiting notes

--- www/js/app.js
+++ www/js/app.js
@@ -1,25 +1,6 @@
-// Ionic Starter App
+(function() {
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 var app = angular.module('starter', ['ionic']);
-
-app.controller('ListsCtrl', function($scope) {
-
-  $scope.notes = [
-    {
-      title: 'First Note',
-      description: 'This is my first note'
-    },
-
-    {
-      title: 'Second Note',
-      description: 'This is my second note'
-    }
-  ];
-});
-
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
@@ -28,24 +9,85 @@ app.config(function($stateProvider, $urlRouterProvider) {
     templateUrl: 'templates/list.html'
   });
 
-  $stateProvider.state('edit', {
-    url: '/edit',
-    templateUrl: 'templates/edit.html'
+  $stateProvider.state('add', {
+    url: '/add',
+    templateUrl: 'templates/edit.html',
+    controller: 'AddCtrl'
   });
 
+  $stateProvider.state('edit', {
+    url: '/edit/:noteId',
+    templateUrl: 'templates/edit.html',
+    controller: 'EditCtrl'
+  });
 
   $urlRouterProvider.otherwise('/list');
 });
 
+var notes = [];
+
+function getNote(noteId) {
+  for (var i = 0; i < notes.length; i++) {
+    if (notes[i].id === noteId) {
+      return notes[i];
+    }
+  }
+  return undefined;
+}
+
+function updateNote(note) {
+  for (var i = 0; i < notes.length; i++) {
+    if (notes[i].id === note.id) {
+      notes[i] = note;
+      return;
+    }
+  }
+}
+
+function createNote(note) {
+  notes.push(note);
+}
+
+app.controller('ListCtrl', function($scope) {
+
+  $scope.notes = notes;
+
+});
+
+
+app.controller('AddCtrl', function($scope, $state) {
+
+  $scope.note = {
+    id: new Date().getTime().toString(),
+    title: '',
+    description: ''
+  };
+
+  $scope.save = function() {
+    createNote($scope.note);
+    $state.go('list');
+  };
+});
+
+app.controller('EditCtrl', function($scope, $state) {
+
+  $scope.note = angular.copy(getNote($state.params.noteId));
+
+  $scope.save = function() {
+    updateNote($scope.note);
+    $state.go('list');
+  };
+});
+
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+}());
